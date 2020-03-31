d027c4d94558050b16033bcdcfb0d0cf1418e361
AnjaliPare <anjali.pare@gmail.com>
2020-03-31 17:13:19 -0500

    program to test pipes

--- redir/main.cpp
+++ redir/main.cpp
@@ -1,10 +1,11 @@
 
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
-	int number;
-	std::cin>>number;
+	std::string number;
+	getline(std::cin,number);
 	std::cout<<"number: "<<number;
 	std::cout<<"Hello World\n";
 }
